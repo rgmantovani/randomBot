c0390cdfd4e82a12b5ee51683dc69ea37a73afa7
Rafael G. Mantovani <rgmantovani@gmail.com>
2016-09-20 16:06:16 +0200

    checking setup packages

--- setup.R
+++ setup.R
@@ -1,16 +1,17 @@
 # -------------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------------
 
-# install required packages
+# install required packages from CRAN
 
 install.packages(pkgs = c("devtools, RWeka", "e1071", "irace", "checkmate", "BatchExperiments",
   "rpart", "ranger", "gbm", "deepNet", "kknn"))
 
+# install required packages from github repos
 library("devtools")
 
 install_github("berndbischl/ParamHelpers", ref = "9d374430701d94639cc78db84f91a0c595927189")
 install_github("mlr-org/mlr", ref = "e8801bcb5ad1ac7c5fdfc7b862df7eb4c37a698a")
-install_github("mlr-org/mlrMBO", ref = "#98dacf7cff03426cc64264686d9611bb0a99ea83")
+install_github("openml/r", ref = "05b8b97cc5ce6ea1b3f586818cfcf157b16a3cd4")
 
 
 # -------------------------------------------------------------------------------------------------
